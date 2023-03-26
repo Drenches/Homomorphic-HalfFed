ba76567c0ac373c8d9237aff81143977580aef45
ljy <lijy573@mail2.sysu.edu.cn>
2023-03-26 14:22:34 +0000

    updated

--- mnist_train.py
+++ mnist_train.py
@@ -8,7 +8,7 @@ torch.manual_seed(73)
 train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
 test_data = datasets.MNIST('data', train=False, download=True, transform=transforms.ToTensor())
 
-batch_size = 64
+batch_size = 32
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
